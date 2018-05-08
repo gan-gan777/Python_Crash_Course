473a7ddd44dad4738a0f14015fef2fbbe9669b0e
shot_gan <shot_gan@hotmail.com>
2018-05-08 17:33:31 +0800

    13.8

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,17 +6,24 @@ class Settings():
 	def __init__(self):
 		"""初始化游戏的设置"""
 		# 屏幕设置
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 800
+		self.screen_height = 1200
 		# self.bg_color = (230, 230, 230)
 		self.bg_color = (5, 5, 5)
 
 		# 飞船的设置
-		self.ship_speed_factor = 1.5
+		self.ship_speed_factor = 10
+		self.ship_limit = 3
 
 		# 子弹设置
-		self.bullet_speed_factor = 1
+		self.bullet_speed_factor = 10
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 255, 55, 83
-		self.bullets_allowed = 3
+		self.bullets_allowed = 6
+
+		# 外星人设置
+		self.alien_speed_factor = 5
+		self.fleet_drop_speed = 10
+		# fleet_direction为1表示向右移，为-1表示向左移
+		self.fleet_direction = 1
